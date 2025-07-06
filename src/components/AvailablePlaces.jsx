ffb9364ab2d20aa95c8a44fb37b9baf2433258b3
P.A.J.U.Pallewela <janithup10@gmail.com>
2025-07-06 21:50:30 +0530

    feat: implement fetching and displaying available places

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,26 @@
+
+
+import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 
+
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [AvailablePlaces, setAvalablePlaces] = useState([]);
+
+  useEffect(()=>{
+    async function fetchPlaces(){
+      const response = await fetch('http://localhost:3000/places');
+      const restData = await response.json();
+      setAvalablePlaces(restData.places);
+
+    }
+    fetchPlaces();
+  },[]);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={AvailablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
